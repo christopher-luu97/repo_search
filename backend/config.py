7a336d7b33fdff1f25e7c2c1b661f57a6d4fea2e
chrluu97 <chrluu.dtt@gmail.com>
2023-10-22 11:21:50 +1100

    Edit Marqo endpoint hardcoding

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,7 @@ def load_settings() -> Path:
     with open("file_path_settings.json", "r") as file:
         settings = json.load(file)
         base_directory = settings.get("FILE_PATHS")
+        marqo_endpoint = settings.get("MARQO_ENDPOINT")
 
     if base_directory:
         base_path = Path(base_directory)
@@ -13,10 +14,10 @@ def load_settings() -> Path:
             print(
                 f"Warning: The path {base_path} does not exist or is not a directory."
             )
-        return base_path
+        return base_path, marqo_endpoint
     else:
         print("Warning: File path not found in settings.")
         return None
 
 
-BASE_PATH = load_settings()
+BASE_PATH, MARQO_ENDPOINT = load_settings()
--- backend/vector_db.py
+++ backend/vector_db.py
@@ -1,5 +1,6 @@
 from vector_database.vectorDatabase import VectorDatabase
+from config import MARQO_ENDPOINT
 
 vector_database = VectorDatabase()
-vector_database.start(endpoint="http://192.168.8.188:8882")
+vector_database.start(endpoint=MARQO_ENDPOINT)
 vector_database.get_index()
