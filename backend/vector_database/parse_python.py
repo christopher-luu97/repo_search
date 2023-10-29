9f1d5bbe9d4b01042b2f3e37c7cfcb59de7474d9
Christopher Luu <71316913+christopher-luu97@users.noreply.github.com>
2023-10-29 16:37:46 +1100

    Update parse_python.py

--- backend/vector_database/parse_python.py
+++ backend/vector_database/parse_python.py
@@ -6,11 +6,5 @@ Language.build_library(
     # Include one or more languages
     [
         "tree-sitter-python",
-        "tree-sitter-dockerfile",
-        "tree-sitter-typescript",
-        "tree-sitter-javascript",
-        "tree-sitter-json",
-        "tree-sitter-css",
-        "tree-sitter-html",
     ],
 )
